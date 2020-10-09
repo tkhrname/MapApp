88ec7129de177008a38b33a5a5f06a2f5dd87615
tkhrname <takehiro.watanabe.224@gmail.com>
2020-10-09 15:57:35 +0900

    first commit

---
+++ MapApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapApp
+//
+//  Created by 渡邊丈洋 on 2020/10/09.
+//  Copyright © 2020 渡邊丈洋. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
